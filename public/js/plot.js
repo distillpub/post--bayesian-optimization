cbd1ee6cb61b2c2c5a9363807ab6871919950deb
apoorvagni@kubuntu <apoorvagni@hotmail.com>
2019-06-24 15:27:58 +0530

    updates

---
+++ public/js/plot.js
@@ -0,0 +1,68 @@
+    // https://gist.github.com/d3noob/b3ff6ae1c120eea654b5
+    // Set the dimensions of the canvas / graph
+function updateplot() {
+    document.getElementById("Teaser").innerHTML="";
+    var margin = {top: 30, right: 20, bottom: 30, left: 50},
+        width = 600 - margin.left - margin.right,
+        height = 270 - margin.top - margin.bottom;
+
+    // Parse the date / time
+    var parseDate = d3.time.format("%d-%b-%y").parse;
+
+    // Set the ranges
+    var eps = document.getElementById("slider").value;
+    console.log(eps);
+    var x = d3.time.scale().range([0, width]);
+    var y = d3.scale.linear().range([height, 0]);
+
+    // Define the axes
+    var xAxis = d3.svg.axis().scale(x)
+        .orient("bottom").ticks(5);
+
+    var yAxis = d3.svg.axis().scale(y)
+        .orient("left").ticks(5);
+
+    // Define the line
+    var valueline = d3.svg.line()
+        .x(function(d) { return x(d.date); })
+        .y(function(d) { return y(d.close); });
+        
+    // Adds the svg canvas
+    var svg = d3.select("#Teaser")
+        .append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+            .attr("transform", 
+                "translate(" + margin.left + "," + margin.top + ")");
+
+    // Get the data
+    d3.csv("./data/data.csv", function(data) {
+        console.log('Hello data here: ' + data);
+        data.forEach( function(d) {
+            d.date = parseDate(d.date);
+            d.close = +d.close;
+        });
+
+    // Scale the range of the data
+    x.domain(d3.extent(data, function(d) { return d.date; }));
+    y.domain([0, d3.max(data, function(d) { return d.close; })]);
+
+    // Add the valueline path.
+    svg.append("path")
+        .attr("class", "line")
+        .attr("d", valueline(data));
+
+    // Add the X Axis
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
+
+    // Add the Y Axis
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis);
+
+    });
+}
