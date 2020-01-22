4a7aeacedd13eef6479afc3a9d00a702762ba694
apoorvagni@kubuntu <apoorvagni@hotmail.com>
2020-01-23 00:14:35 +0530

    new_public

--- public/js/hider.js
+++ public/js/hider.js
@@ -7,10 +7,8 @@ for (i = 0; i < coll.length; i++) {
     var content = this.nextElementSibling;
     if (content.style.maxHeight){
       content.style.maxHeight = null;
-      content.style.borderStyle = "hidden"
     } else {
       content.style.maxHeight = content.scrollHeight + "px";
-      content.style.borderStyle = "ridge"
     } 
   });
 }
